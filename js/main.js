b9dc395d6f2477d24bec6d298874be29aacb3180
Daria <fordaryapopova@yandex.ru>
2019-07-09 15:37:49 +0300

    Перетаскивание маркера

--- js/main.js
+++ js/main.js
@@ -160,10 +160,41 @@ var formTimeOutElement = document.querySelector('#timeout');
 var avatarURLs = createAvatarURLs(AVATARS_LIMIT);
 var pins = createPins(PINS_LIMIT);
 
-mapPinMainElement.addEventListener('mouseup', function () {
+mapPinMainElement.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
   activatePage();
   renderPins(pins);
-  addStartPinCoordinates();
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  function onMouseMove(moveEvt) {
+    moveEvt.preventDefault();
+    addStartPinCoordinates();
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mapPinMainElement.style.top = (mapPinMainElement.offsetTop - shift.y) + 'px';
+    mapPinMainElement.style.left = (mapPinMainElement.offsetLeft - shift.x) + 'px';
+  }
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
 });
 
 formTypeElement.addEventListener('change', function (evt) {
