87d1d8d379f52338980ca2704a71a739a0ac86ab
Daria <fordaryapopova@yandex.ru>
2019-07-24 15:42:24 +0300

    Синхронизация полей кол-во комнат и кол-во гостей

--- js/form.js
+++ js/form.js
@@ -33,6 +33,9 @@
   var formPriceElement = document.querySelector('#price');
   var formTimeInElement = document.querySelector('#timein');
   var formTimeOutElement = document.querySelector('#timeout');
+  var formRoomNumberElement = document.querySelector('#room_number');
+  var formCapacityElement = document.querySelector('#capacity');
+  var formCapacityElements = Array.from(formCapacityElement.querySelectorAll('option'));
   var addFormElement = document.querySelector('.ad-form');
   var adFormFieldsets = addFormElement.querySelectorAll('fieldset');
   var addFormAddressInputElement = addFormElement.querySelector('input[name="address"]');
@@ -51,6 +54,45 @@
 
   updateFormPriceAttributes(formTypeElement.value);
 
+  function setRoomsCapacityStartValue() {
+    formRoomNumberElement.item(0).setAttribute('selected', 'selected');
+    formCapacityElements.forEach(function (item) {
+      item.setAttribute('disabled', 'disabled');
+      item.removeAttribute('selected', 'selected');
+    });
+    formCapacityElement.item(2).setAttribute('selected', 'selected');
+    formCapacityElement.item(2).removeAttribute('disabled', 'disabled');
+
+  }
+
+  formRoomNumberElement.addEventListener('change', function () {
+    formCapacityElements.forEach(function (item) {
+      item.setAttribute('disabled', 'disabled');
+      item.removeAttribute('selected', 'selected');
+    });
+
+    switch (formRoomNumberElement.value) {
+      case '1':
+        formCapacityElement.item(2).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(2).setAttribute('selected', 'selected');
+        break;
+      case '2':
+        formCapacityElement.item(1).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(2).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(1).setAttribute('selected', 'selected');
+        break;
+      case '3':
+        formCapacityElement.item(0).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(1).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(2).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(0).setAttribute('selected', 'selected');
+        break;
+      case '100':
+        formCapacityElement.item(3).removeAttribute('disabled', 'disabled');
+        formCapacityElement.item(3).setAttribute('selected', 'selected');
+    }
+  });
+
   window.form = {
     activate: function () {
       addFormElement.classList.remove('ad-form--disabled');
@@ -59,6 +101,7 @@
     deactivate: function () {
       addFormElement.classList.add('ad-form--disabled');
       addDisabledAttributes(adFormFieldsets);
+      setRoomsCapacityStartValue();
     },
     setCoordinates: function (value) {
       addFormAddressInputElement.value = value;
