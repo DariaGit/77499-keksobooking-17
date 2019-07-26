dcd208a9a7269730066fda4c129c9eef4e89a6c4
Daria <fordaryapopova@yandex.ru>
2019-07-26 19:01:08 +0300

    Добавлены обработчики клика по клавише Esc и клика по свободной области

--- js/success.js
+++ js/success.js
@@ -1,12 +1,41 @@
 'use strict';
 
 (function () {
+  var KEY_CODE_ESC = 27;
+
   var mainElement = document.querySelector('main');
   var successTemplateElement = document.querySelector('#success').content.querySelector('.success');
   var successMessageNodeElement = successTemplateElement.cloneNode(true);
 
+  function closeMessage() {
+    successMessageNodeElement.remove();
+    destroyListeners();
+  }
+
+  function onDocumentKeydown(evt) {
+    evt.preventDefault();
+    if (evt.keyCode === KEY_CODE_ESC) {
+      closeMessage();
+    }
+  }
+
+  function onDocumentClick() {
+    closeMessage();
+  }
+
+  function createListeners() {
+    document.addEventListener('keydown', onDocumentKeydown);
+    document.addEventListener('click', onDocumentClick);
+  }
+
+  function destroyListeners() {
+    document.removeEventListener('keydown', onDocumentKeydown);
+    document.removeEventListener('click', onDocumentClick);
+  }
+
   window.success = {
     create: function () {
+      createListeners();
       mainElement.appendChild(successMessageNodeElement);
     }
   };
